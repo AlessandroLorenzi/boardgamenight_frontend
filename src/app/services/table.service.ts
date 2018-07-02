b49173bd4e926e5b08197d23d3b0df8f16f1b466
Alessandro Lorenzi <alessandro@lm-net.it>
2018-07-02 18:43:41 +0200

    Table owner

--- src/app/services/table.service.ts
+++ src/app/services/table.service.ts
@@ -3,26 +3,22 @@ import { Event, Table, Gamer } from '../models';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { environment } from 'environments/environment';
+import { GamerService } from './gamer.service';
 
 @Injectable()
 export class TableService {
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private gamerService: GamerService,
   ) { }
   private apiUrl = environment.apiUrl;
 
   private tableUrl = this.apiUrl + '/table/';
 
-  private httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-    })
-  };
-
   deleteTable (id) {
-    return this.http.delete(this.tableUrl + id);
+    return this.http.delete(this.tableUrl + id, this.gamerService.httpOptions);
   }
   addTable (newTable:Table) {
-    return this.http.post(this.tableUrl + '0', newTable, this.httpOptions);
+    return this.http.post(this.tableUrl + '0', newTable, this.gamerService.httpOptions);
   }
 }
